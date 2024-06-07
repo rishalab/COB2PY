370d5b8abd62b57990d0625aea06dd6cba494eb8
Sonith <muppirivishnusonith@gmail.com>
2024-06-07 21:31:08 +0530

    Symbol table rep

--- main.py
+++ main.py
@@ -4,36 +4,16 @@ from Cobol85Parser import Cobol85Parser
 from CustomVisitor import CustomVisitor
 from SymbolTable import SymbolTable
 
-<<<<<<< HEAD
-file_path = './tests/DIVIDE.cbl'
+file_path = './tests/shop.cbl'
 def main(file_path):
     input_stream = FileStream(file_path)
     lexer = Cobol85Lexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = Cobol85Parser(stream)
     tree = parser.startRule()
-    # print(tree.toStringTree(recog=parser))
-    # print(type(tree))
-    visitor = CustomVisitor(parser)
+    visitor = SymbolTable()
     visitor.visit(tree=tree)
-    
-    
-    print("python code\n")
-    print(visitor.get_python_code())
-    print(visitor.get_symbol_table())
+    print(visitor.__repr__())
 
 if __name__ == '__main__':
     main(file_path)
-=======
-file_path = './tests/shop.cbl'
-
-input_stream = antlr4.FileStream(file_path)
-lexer = Cobol85Lexer(input_stream)
-stream = antlr4.CommonTokenStream(lexer)
-parser = Cobol85Parser(stream)
-tree = parser.startRule()
-print(tree.toStringTree(recog=parser))
-print(type(tree))
-vistor = SymbolTable()
-vistor.visit(tree=tree)
->>>>>>> 03b2440 (Symbol table)
