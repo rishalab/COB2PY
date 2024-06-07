8bc0c5e3a9ef6a55c831753412c6155c417702b9
Kowshiik reddy challa <cs22b015@iittp.ac.in>
2024-06-07 20:41:50 +0530

    Added a symbol table class-------- "Updated .gitignore, added gen to ignore list; modified CustomVisitor.py to add parser and symbol table; updated main.py to create visitor with parser and print python code and symbol table; updated requirements.txt"

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-import antlr4
+from antlr4 import *
 from Cobol85Lexer import Cobol85Lexer
 from Cobol85Parser import Cobol85Parser
 from CustomVisitor import CustomVisitor
 
 file_path = './tests/DIVIDE.cbl'
+def main(file_path):
+    input_stream = FileStream(file_path)
+    lexer = Cobol85Lexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = Cobol85Parser(stream)
+    tree = parser.startRule()
+    # print(tree.toStringTree(recog=parser))
+    # print(type(tree))
+    visitor = CustomVisitor(parser)
+    visitor.visit(tree=tree)
+    
+    
+    print("python code\n")
+    print(visitor.get_python_code())
+    print(visitor.get_symbol_table())
 
-input_stream = antlr4.FileStream(file_path)
-lexer = Cobol85Lexer(input_stream)
-stream = antlr4.CommonTokenStream(lexer)
-parser = Cobol85Parser(stream)
-tree = parser.startRule()
-print(tree.toStringTree(recog=parser))
-print(type(tree))
-vistor = CustomVisitor()
-vistor.visit(tree=tree)
+if __name__ == '__main__':
+    main(file_path)
